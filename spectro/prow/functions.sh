4db9887f2b94b0579ff609ac328292a1a11ecdf1
Deepak Sharma <deepak@spectrocloud.com>
2020-05-20 15:15:08 +0530

    remove space from assignment

--- spectro/prow/functions.sh
+++ spectro/prow/functions.sh
@@ -44,7 +44,7 @@ build_vendor_manifest() {
   ../run.sh
 }
 
-IMG_LOC = "cluster-api/release"
+IMG_LOC="cluster-api/release"
 export IMG_LOC
 export REGISTRY=${DOCKER_REGISTRY}/${IMG_LOC}
 export STAGING_REGISTRY=${DOCKER_REGISTRY}/${IMG_LOC}
