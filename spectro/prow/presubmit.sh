415324f2b5cacf19f0c28a91ee377cba9a0a478b
Deepak Sharma <deepak@spectrocloud.com>
2020-05-21 20:03:14 +0530

    presubmit script

---
+++ spectro/prow/presubmit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+########################################
+# Presubmit script triggered by Prow.  #
+########################################
+
+WD=$(dirname $0)
+WD=$(cd $WD; pwd)
+ROOT=$(dirname $WD)
+source spectro/prow/functions.sh
+
+# Exit immediately for non zero status
+set -e
+# Check unset variables
+set -u
+# Print command trace
+set -x
+
+
+create_images
+
+exit 0
