01f5f381ceafb7a0293c578c89a0d6f3f6174739
abhinavnagaraj <abhinav@spectrocloud.com>
2020-10-28 21:53:49 -0700

    generate spectro manifests

---
+++ spectro/prow/functions.sh
@@ -0,0 +1,49 @@
+print_step() {
+	text_val=$1
+	set +x
+	echo " "
+	echo "###################################################
+#  ${text_val}
+###################################################"
+	echo " "
+	set -x
+}
+
+
+set_image_tag() {
+	IMG_TAG="latest"
+
+	if [[ ${JOB_TYPE} == 'presubmit' ]]; then
+	    VERSION_SUFFIX="-dev"
+	    IMG_LOC='pr'
+	    IMG_TAG=${PULL_NUMBER}
+        fi
+	if [[ ${JOB_TYPE} == 'periodic' ]]; then
+	    VERSION_SUFFIX="-$(date +%m%d%y)"
+	    IMG_LOC='daily'
+	    IMG_TAG=$(date +%Y%m%d.%H%M)
+	fi
+	if [[ ${SPECTRO_RELEASE} ]] && [[ ${SPECTRO_RELEASE} == "yes" ]]; then
+	    export VERSION_SUFFIX=""
+	    IMG_LOC='release'
+	    IMG_TAG=$(make get-version)
+	fi
+
+	export PROD_BUILD_ID
+	export IMG_TAG
+	export VERSION_SUFFIX
+}
+
+create_images() {
+	print_step "Create and Push the images"
+	make docker-build-all
+}
+
+build_vendor_manifest() {
+  print_step "Build vendor manifest for cluster-api"
+  ../run.sh
+}
+
+export REGISTRY=${DOCKER_REGISTRY}/${IMG_LOC}
+export STAGING_REGISTRY=${DOCKER_REGISTRY}/${IMG_LOC}
+export PROD_REGISTRY=${DOCKER_REGISTRY}/${IMG_LOC}
