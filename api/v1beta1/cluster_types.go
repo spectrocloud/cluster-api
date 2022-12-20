e636a859755560abee20b8c89db90b1348743485
Amit Sahastrabuddhe <amit.sahastra@gmail.com>
2022-12-20 17:22:14 +0530

    Added takeOverCluster in ClusterSpec

--- api/v1beta1/cluster_types.go
+++ api/v1beta1/cluster_types.go
@@ -68,6 +68,10 @@ type ClusterSpec struct {
 	// this feature is highly experimental, and parts of it might still be not implemented.
 	// +optional
 	Topology *Topology `json:"topology,omitempty"`
+
+	//TODO: PCP-22, create/set cluster object for imported/takeover cluster
+	// TakeOverCluster represents the cluster launch is for taking over existing pre-provisioned cluster
+	TakeOverCluster bool `json:"takeOverCluster,omitempty"`
 }
 
 // Topology encapsulates the information of the managed resources.
