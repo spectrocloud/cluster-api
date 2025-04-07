593603571f4306a7a20c396c613300a38076efd7
kun zhou <kun.zhou@spectrocloud.com>
2025-04-07 10:40:54 -0700

    takeover related changes for CAPI v1.3.2

--- controlplane/kubeadm/internal/controllers/controller.go
+++ controlplane/kubeadm/internal/controllers/controller.go
@@ -394,6 +394,11 @@ func (r *KubeadmControlPlaneReconciler) reconcile(ctx context.Context, cluster *
 	switch {
 	// We are creating the first replica
 	case numMachines < desiredReplicas && numMachines == 0:
+		if annotations.IsTakeOverCluster(controlPlane.Cluster.GetObjectMeta()) {
+			// Create a new Machine w/ join
+			log.Info("Scaling up control plane for TakeOverCluster", "Desired", desiredReplicas, "Existing", numMachines)
+			return r.scaleUpControlPlane(ctx, cluster, kcp, controlPlane)
+		}
 		// Create new Machine w/ init
 		log.Info("Initializing control plane", "Desired", desiredReplicas, "Existing", numMachines)
 		conditions.MarkFalse(controlPlane.KCP, controlplanev1.AvailableCondition, controlplanev1.WaitingForKubeadmInitReason, clusterv1.ConditionSeverityInfo, "")
@@ -568,7 +573,7 @@ func (r *KubeadmControlPlaneReconciler) reconcileEtcdMembers(ctx context.Context
 	log := ctrl.LoggerFrom(ctx)
 
 	// If etcd is not managed by KCP this is a no-op.
-	if !controlPlane.IsEtcdManaged() {
+	if annotations.IsTakeOverCluster(controlPlane.Cluster.GetObjectMeta()) || !controlPlane.IsEtcdManaged() {
 		return ctrl.Result{}, nil
 	}
 
